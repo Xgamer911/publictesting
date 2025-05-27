b8023f3460ef469ae806c9bc814ab261880faa4f
Xgamer911 <Xgamer911@hotmail.com>
2025-05-26 20:19:51 -0400

    Add files via upload

---
+++ MyTesting/test1.ps1
@@ -0,0 +1,12 @@
+﻿Add-Type -AssemblyName System.Device
+$geoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher
+$geoWatcher.Start()
+Start-Sleep -Seconds 2  # wait for location fix
+$coord = $geoWatcher.Position.Location
+if ($coord.IsUnknown) {
+    Write-Output "Location unknown"
+} else {
+    Write-Output "Latitude: $($coord.Latitude)"
+    Write-Output "Longitude: $($coord.Longitude)"
+}
+$geoWatcher.Stop()
